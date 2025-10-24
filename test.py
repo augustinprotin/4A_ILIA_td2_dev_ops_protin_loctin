e23eb12e655bef40068980ff9a57d9a5c0d2a7b5
Thomas Loctin <thomas.loctin@gmail.com>
2025-10-24 11:40:44 +0200

    test: create_event()

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import main
 import calendar as C
+from datetime import datetime
 
 
 def test_hello():
     assert main.hello() == "Hello world !"
+
+
+def test_create_event():
+    date = datetime(2025, 10, 24)
+    assert C.create_event(date, 2, "Event") == (date, 2, "Event")
