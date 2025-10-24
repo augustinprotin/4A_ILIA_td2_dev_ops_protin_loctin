d651a24ebe19977481445a489fea24c11960caa6
Thomas Loctin <thomas.loctin@gmail.com>
2025-10-24 13:43:09 +0200

    test: get_sorted_events()

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ import main
 import calendrier as C
 from datetime import datetime
 
-date = datetime(2025, 10, 24)
+date1 = datetime(2025, 10, 24)
+date2 = datetime(2026, 10, 24)
+date3 = datetime(2024, 10, 24)
+ev1 = C.create_event(date1, 2, "Event 1")
+ev2 = C.create_event(date2, 2, "Event 2")
 
 
 def test_hello():
@@ -10,8 +14,12 @@ def test_hello():
 
 
 def test_create_event():
-    assert C.create_event(date, 2, "Event") == (date, 2, "Event")
+    assert C.create_event(date3, 2, "Event 3") == (date3, 2, "Event 3")
 
 
 def test_get_events():
-    assert C.get_events() == [(date, 2, "Event")]
+    assert C.get_events() == [ev1, ev2, (date3, 2, "Event 3")]
+
+
+def test_get_sorted_events():
+    assert C.get_sorted_events() == [(date3, 2, "Event 3"), ev1, ev2]
