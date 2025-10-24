d3b8147f78df4c135f25bca34f70d0da992e4836
Augustin <119314433+augustinprotin@users.noreply.github.com>
2025-10-24 09:50:29 +0000

    creation of the events and calendar create events functions

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 def hello(msg="Hello World !"):
     return msg
-def test():
-    return 0
+T1 = 0
+list_of_events = []
 
+def calendar_create_event():
+    t = input("combien de seconde depuis le timestamp ?\n")
+    n = input("un nom stp\n")
+    date = (T1,int(t),n)
+    list_of_events.append(date)
+    return date
+
+
+def events():
+    for i in list_of_events :
+        print(i)
+    return list_of_events
+
+calendar_create_event()
+calendar_create_event()
+events()
