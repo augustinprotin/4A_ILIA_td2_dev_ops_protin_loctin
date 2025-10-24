524c454bc01894bf0e213fe5369bf4585367db24
Thomas Loctin <thomas.loctin@gmail.com>
2025-10-24 11:54:40 +0200

    test: get_events()

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ import main
 import calendrier as C
 from datetime import datetime
 
+date = datetime(2025, 10, 24)
+
 
 def test_hello():
     assert main.hello() == "Hello world !"
 
 
 def test_create_event():
-    date = datetime(2025, 10, 24)
     assert C.create_event(date, 2, "Event") == (date, 2, "Event")
+
+
+def test_get_events():
+    assert C.get_events() == [(date, 2, "Event")]
